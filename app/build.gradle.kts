56afac11962fb446cd40ce3c6a4dac258ec8d6c2
Kadir Çetin <kadir.cetin72@gmail.com>
2026-06-17 10:56:02 +0300

    USD model is added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,9 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -46,4 +49,14 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    // Gson Converter
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+    // Lifecycle Scope
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
+    // RecyclerView
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
 }
---
+++ app/src/main/java/com/kadircetin/cryptotracker/model/USD.kt
@@ -0,0 +1,3 @@
+package com.kadircetin.cryptotracker.model
+
+data class USD(val price: Double)
